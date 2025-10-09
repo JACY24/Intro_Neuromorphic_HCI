a0b8a6882c4f847030c1e36f357c94dbec00f7c8
Jord Cluitmans <36312448+JACY24@users.noreply.github.com>
2025-10-09 14:52:12 +0200

    Revert "Config from json"

--- Intro_Neuromorphic_HCI/main.py
+++ Intro_Neuromorphic_HCI/main.py
@@ -1,13 +1,10 @@
 import program as prog
 import experiment as exp
-import sys
-from pathlib import Path
 
 def main():
-    sys.path.insert(0, str(Path(__file__).parent / "libpointing/bindings/Python/cython"))
-
+    # experiment = exp.Experiment()
     program = prog.Program()
-    program.run()
+    program.run()   
 
 if __name__ == "__main__":
     main()
